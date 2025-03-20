ebdfddeb5ca0506645650b5cac4e2783efc7ec3c
Pablo Pérez <46758902+pa-perz@users.noreply.github.com>
2025-03-20 10:24:45 +0000

    Improve grabbing interaction

--- entities/player/interaction/Grab.gd
+++ entities/player/interaction/Grab.gd
@@ -2,8 +2,10 @@ extends PlayerInteractionState
 
 @export var impulse_strength := 5.0
 
+
 func enter(previous_state_path: String, data := {}) -> void:
 	player.interaction_panel.hide_ui()
+
 
 func physics_update(delta: float) -> void:
 	if Input.is_action_just_pressed("interact"):
@@ -12,6 +14,7 @@ func physics_update(delta: float) -> void:
 		var impulse_direction =  player.camera_controller.global_transform.origin.direction_to(player.object.global_transform.origin).normalized()
 		player.object.apply_impulse(impulse_direction * impulse_strength)
 		finished.emit(IDLE)
+		
 
 
 func exit() -> void:
--- interfaces/InteractableObject.gd
+++ interfaces/InteractableObject.gd
@@ -13,25 +13,29 @@ var selected_interaction: int = 0
 var is_grabbed = false
 var target_position: Vector3
 var move_force: float = 20.0
-@export var stiffness: float = 50.0  # Spring strength
-@export var damping: float = 10.0    # Damping to reduce overshoot
-
+@export var stiffness: float = 50.0
+@export var damping: float = 10.0
 
 
 func _ready() -> void:
 	selected_interaction = 0 if interactions and !interactions.is_empty() else null
-	freeze_mode = RigidBody3D.FREEZE_MODE_KINEMATIC
-	
+
+func _input(event: InputEvent) -> void:
+	if not is_grabbed:
+		return
+
 
 func _physics_process(delta: float) -> void:
-	if is_grabbed:
-		var camera_transform = Global.player.camera_controller.global_transform
-		target_position = camera_transform.translated_local(grab_distance).origin
-		
-		var distance = global_transform.origin.distance_to(target_position)
-		var direction = (target_position - global_transform.origin).normalized()
-		
-		apply_central_force(direction * move_force * clamp(distance, 0, 1))
+	if not is_grabbed:
+		return
+	
+	var camera_transform = Global.player.camera_controller.global_transform
+	target_position = camera_transform.translated_local(grab_distance).origin
+	
+	var distance = global_transform.origin.distance_to(target_position)
+	var direction = (target_position - global_transform.origin).normalized()
+	
+	apply_central_force(direction * move_force * clamp(distance, 0, 1))
 
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
